13bf93ff007aebe234ba254ff1676ba6207129c2
Shinya Takeuchi <bamboo@rj.dream.jp>
2017-03-20 00:59:40 +0900

    #1 For JSON-Ajax

--- src/sample/sample_elem1.cpp
+++ src/sample/sample_elem1.cpp
@@ -4,7 +4,7 @@
 StkObject* Sample_Elem1::Execute(StkObject* ReqObj, int Method, TCHAR UrlPath[128], int* ResultCode)
 {
 	int ErrorCode;
-	StkObject* TmpObj = StkObject::CreateObjectFromXml(_T("<body><h1>Hello, 1</h1></body>"), &ErrorCode);
+	StkObject* TmpObj = StkObject::CreateObjectFromJson(_T("{ \"Elem1\" : { \"bbb\" : 123, \"ccc\" : \"xyz\" }, \"ddd\" : 987 }"), &ErrorCode);
 	*ResultCode = 200;
 	return TmpObj;
 }
--- src/sample/sample_elem3.cpp
+++ src/sample/sample_elem3.cpp
@@ -4,7 +4,7 @@
 StkObject* Sample_Elem3::Execute(StkObject* ReqObj, int Method, TCHAR UrlPath[128], int* ResultCode)
 {
 	int ErrorCode;
-	StkObject* TmpObj = StkObject::CreateObjectFromXml(_T("<body><h1>Hello, 3</h1></body>"), &ErrorCode);
+	StkObject* TmpObj = StkObject::CreateObjectFromJson(_T("{ \"Elem3\" : { \"bbb\" : 123, \"ccc\" : \"xyz\" }, \"ddd\" : 987 }"), &ErrorCode);
 	*ResultCode = 200;
 	return TmpObj;
 }
