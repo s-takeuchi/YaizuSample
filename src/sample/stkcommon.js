21cdd92a8716e66c6e462a90390640859ac8a39b
Shinya Takeuchi <bamboo@rj.dream.jp>
2019-06-08 12:19:05 +0900

    #19 : For waiting response dialog

--- src/sample/stkcommon.js
+++ src/sample/stkcommon.js
@@ -9,6 +9,22 @@ var underComm = 0;
 var statusCode = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var responseData = [{},{},{},{},{},{},{},{},{},{},{},{},{},{}];
 
+// Init flag of loading modal
+var initLoadingModalFlag = false;
+
+function initLoadingModal() {
+    var loadingModal = $('<div id="loading_Modal" class="modal fade" tabindex="-1" role="dialog" data-keyboard="false" data-backdrop="static">');
+    var modalDialog = $('<div class="modal-dialog">');
+    var modalContent = $('<div class="modal-content">');
+    var modalHeader = $('<div class="modal-header" style="padding: 0px; text-align: center;"><h3 style="margin-top: 5px;">Now loading ...</h3></div>');
+    var modalBody = $('<div class="modal-body"><div style="height:20px"><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:100%"> </div></div></div></div>');
+
+    modalContent.append(modalHeader);
+    modalContent.append(modalBody);
+    modalDialog.append(modalContent);
+    loadingModal.append(modalDialog);
+    $('body').append(loadingModal);
+}
 
 function apiCall(method, url, request, index, targetFunc) {
     if (method != null && url != null && index != -1) {
@@ -20,6 +36,10 @@ function apiCall(method, url, request, index, targetFunc) {
 }
 
 function waitForResponse(count, targetFunc) {
+    if (initLoadingModalFlag == false) {
+        initLoadingModalFlag = true;
+        initLoadingModal();
+    }
     if (targetFunc == null) {
         return;
     }
@@ -59,9 +79,14 @@ function sendRequestRecvResponse(method, url, request, index) {
             error: function(xhr, textStatus, errorThrown) {
                 if (textStatus === 'timeout') {
                     statusCode[index] = -1;
+                    responseData[index] = {};
                 } else {
                     statusCode[index] = xhr.status;
-                    $.extend(responseData[index], JSON.parse(xhr.responseText));
+                    try {
+                        $.extend(responseData[index], JSON.parse(xhr.responseText));
+                    } catch (e) {
+                        responseData[index] = {};
+                    }
                 }
                 underComm--;
             }
@@ -84,9 +109,14 @@ function sendRequestRecvResponse(method, url, request, index) {
             error: function(xhr, textStatus, errorThrown) {
                 if (textStatus === 'timeout') {
                     statusCode[index] = -1;
+                    responseData[index] = {};
                 } else {
                     statusCode[index] = xhr.status;
-                    $.extend(responseData[index], JSON.parse(xhr.responseText));
+                    try {
+                        $.extend(responseData[index], JSON.parse(xhr.responseText));
+                    } catch (e) {
+                        responseData[index] = {};
+                    }
                 }
                 underComm--;
             }
